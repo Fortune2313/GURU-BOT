ed0826e8ef87613129b6f061323a2fe738a24bf8
Fortune2313 <148874225+Fortune2313@users.noreply.github.com>
2023-10-25 10:10:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2348122501507', 'LAMILTON', true],
+  ['2348122501507', 'TAESHAWN', true], 
   [''] 
 ] //Number of owners
 
